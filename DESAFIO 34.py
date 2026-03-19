7058efca6bf3c42618f8b122448d5a6897538eca
LucasGroth <lucaslwgo@gmail.com>
2026-03-19 10:30:49 -0300

    Encerrando mundo 1

--- DESAFIO 34.py
+++ DESAFIO 34.py
@@ -1,3 +1,8 @@
 #Um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 #Para salários acima de 1250 calcule um aumento de 10%.
 #Para inferiores ou iguais o aumento será de 15%.
+salario=float(input('Informe o salário do funcionário:'))
+if salario <= 1250.00:
+    print('O novo salário do funcionário pós aumento de 15% será de R${:.2f}.'.format(salario + (salario*0.15)))
+else:
+    print('O novo salário do funcionário pós aumento de 10% será de R${:.2f}.'.format(salario*0.10+salario))
--- DESAFIO 35.py
+++ DESAFIO 35.py
@@ -1,2 +1,34 @@
 #Um programa que leia o comprimento de três retas e diga ao usuário
 # se elas podem ou não formar um triângulo.
+#Somar as duas menores retas e a soma delas tem que ser maior que a maior reta. (usaremos 3,4 e 5)
+ra=int(input('Informe o tamanho da primeira reta:'))
+rb=int(input('Informe o tamanho da segunda reta:'))
+rc=int(input('Informe o tamanho da terceira reta'))
+#primeiro vamos descobrir qual a maior reta e quais as menores.
+menor=ra
+if rb<ra and rb<rc:
+    menor=rb
+if rc<ra and rc<rb:
+    menor=rc
+#Agora calculamos o maior
+maior=ra
+if rb>ra and rb>rc:
+    maior=rb
+if rc>ra and rc>rb:
+    maior=rc
+#Agora precisamos fazer o computador identificar o segundo menor número
+segundo=ra
+if rb<ra and rb>rc:
+    segundo=rb
+if rc<ra and rc>rb:
+    segundo=rc
+#Agora o computador deve calcular as retas e verificar a condição de existencia de triangulo.
+if menor+segundo>maior:
+    print('Com essas retas informadas é possível sim formar um triangulo')
+else:
+    print('Com essas retas informadas não é possível formar um triangulo')
+#METODO  DO GUANABARA KKKKKK Muito mais simples
+if ra<rb + rc and rb<ra + rc and rc<ra + rb:
+    print('Forma triangulo')
+else:
+    print('Não forma triangulo')
